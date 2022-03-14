65f7c93cc40711518f54a4fe250f61a3761014b7
BW <sevenvsby2@naver.com>
2022-03-14 22:34:21 +0900

    0314 commit

--- 0313/BOJ_#14503/solution.py
+++ 0313/BOJ_#14503/solution.py
@@ -9,34 +9,36 @@ cur_dict = {0 : 3, 1 : 0, 2 : 1, 3 : 2}
 back_dict = {0 : (1, 0), 1: (0, -1), 2 : (-1, 0), 3 : (0, 1)}
 cnt = 1
 arr[r][c] = 2
-k = -1
 flag = False
 while True:
-    while k < 3:
-        k += 1
-        nr, nc = r + dir_dict[d][0], c + dir_dict[d][1]
-        if arr[nr][nc] == 0:
-            r, c = nr, nc
-            arr[r][c] = 2
-            cnt += 1
-            k = -1
-            d = cur_dict[d]
-            cur_cnt = 0
-        else:
-            if cur_cnt == 4:
-                pass
-            else:
-                d = cur_dict[d]
-                cur_cnt += 1
-
-
-    nr, nc = r + back_dict[d][0], c + back_dict[d][1]
-    if arr[nr][nc] == 1:
-        print(d)
-        print(nr, nc)
-        flag = True
-    else:
-        r, c = nr, nc
     if flag:
+        print(cnt)
         break
-print(cnt)
+    else:
+        temp_d = d
+        for k in range(4):
+            if temp_d == k:
+                nr, nc = r + dir_dict[temp_d][0], c + dir_dict[temp_d][1]
+                if arr[nr][nc] == 0:
+                    r, c = nr, nc
+                    arr[r][c] = 2
+                    cnt += 1
+                    d = cur_dict[temp_d]
+                    # for i in range(N):
+                    #     for j in range(M):
+                    #         print(arr[i][j], end=' ')
+                    #     print()
+                    # print('------------------')
+                    break
+                else:
+                    temp_d = cur_dict[temp_d]
+        else:
+            nr, nc = r + back_dict[d][0], c + back_dict[d][1]
+            if arr[nr][nc] == 2:
+                r, c = nr, nc
+            else:
+                flag = True
+for i in range(N):
+    for j in range(M):
+        print(arr[i][j], end= ' ')
+    print()
--- 0313/SWEA_#4012/solution.py
+++ 0313/SWEA_#4012/solution.py
@@ -2,8 +2,8 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 def comb(r, start, M):
+    global min_val
     if r == M:
-        bits = [0] * M
         lst = nums[:]
         order = []
         order.append(S)
@@ -11,38 +11,30 @@ def comb(r, start, M):
             lst.remove(s)
         order.append(lst)
         A_lst = order[0]; B_lst = order[1]
-        A_total = 0
-        subset(0, M, A_lst, bits, A_total)
-        print(A_total)
+        A_total = B_total = 0
+        for i in range(M-1):
+            for j in range(i + 1, M):
+                A_total += arr[A_lst[i]][A_lst[j]]
+                A_total += arr[A_lst[j]][A_lst[i]]
+        for i in range(M-1):
+            for j in range(i + 1, M):
+                B_total += arr[B_lst[i]][B_lst[j]]
+                B_total += arr[B_lst[j]][B_lst[i]]
+        if min_val > abs(A_total - B_total):
+            min_val = abs(A_total - B_total)
     else:
         for i in range(start, N):
             S.append(nums[i])
             comb(r + 1, i + 1, M)
             S.pop()
 
-def subset(k, n, lst, bits, total):
-    if k == n:
-        if sum(bits) == 2:
-            for i in range(n):
-                if bits[i]:
-                    total += lst[i]
-    else:
-        bits[k] = 0
-        subset(k + 1, n, lst, bits, total)
-        bits[k] = 1
-        subset(k + 1, n, lst, bits, total)
-
-# nums = [1, 2, 3]
-# N = len(nums)
-# bits = [0]*N
-# total = 0
-# subset(0, N, nums)
-#
 T = int(input())
 for test_case in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
     nums = list(range(N))
     S = []
+    min_val = 200000
     M = N // 2
     comb(0, 0, M)
+    print(f'#{test_case} {min_val}')
